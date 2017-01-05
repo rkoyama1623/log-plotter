b0e1a5aa9cdd8f890f49afa05af64bcdfb0371c4
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2017-01-06 02:09:09 +0900

    [setup.py, src/log_plotter/__init__.py] add version in setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
+from xml.etree import ElementTree
+import os
 
-try: # install in catkin work space
+SCRIPTDIR = os.path.abspath(os.path.dirname(__file__))
+
+# setup log_plotter using distutils
+try:
+    from setuptools import setup
+except ImportError:
     from distutils.core import setup
-    from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
+from catkin_pkg.python_setup import generate_distutils_setup
+from os.path import join
+d=generate_distutils_setup(
+    package_xml_path=SCRIPTDIR,
+    packages=['log_plotter'],
+    package_dir={'': 'src'},
+    scripts=['src/log_plotter/datalogger_plotter_with_pyqtgraph.py'],
+    # depends=['catkin_pkg', 'pyqtgraph', 'metayaml']
+    install_requires=[
+        'catkin_pkg',
+        'pyqtgraph',
+        'metayaml'
+    ],
+)
 
-    d = generate_distutils_setup(
-        packages=['log_plotter'],
-        package_dir={'': 'src'},
-        scripts=['src/log_plotter/datalogger_plotter_with_pyqtgraph.py'],
-    )
-    setup(**d)
-except: # install in /usr/local/
-    print('catkin is not installed. use setuptools instead')
-    from setuptools import setup, find_packages
-    from os.path import join
-    setup(name='log_plotter',
-          version='0.0.0',
-          description='log plotter for hrpsys',
-          packages=[join('src', package) for package in find_packages(where='./src/')],
-          entry_points="""
-          [console_scripts]
-          datalogger-plotter-with-pyqtgraph = log_plotter:main
-          """,)
+# get commit hash
+from subprocess import check_output
+try:
+    GIT_REVISION = check_output(["git", "rev-parse", "HEAD"]).replace("\n","")
+except:
+    GIT_REVISION = ""
+    
+# write into version.py
+with open(os.path.join(SCRIPTDIR, "src","log_plotter","version.py"),"w") as version_py:
+    version_py.writelines("# THIS FILE IS GENERATED FROM LOG_PLOTTER SETUP.PY\n")
+    version_py.writelines("version='{}'\n".format(d["version"]))
+    version_py.writelines("git_revision='{}'\n".format(GIT_REVISION))
 
+setup(**d)
